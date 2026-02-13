957f04cfe2dcbf2fe61067bb8973486bd5911897
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-02-13 15:13:31 +0800

    Create contains function that searches value inside the nested object and array

--- 2_contains/contains.js
+++ 2_contains/contains.js
@@ -1,5 +1,26 @@
-const contains = function() {
-  
+const contains = function(obj, targetValue) {
+  // Recursive Base
+  if (typeof obj !== 'object' || obj === null ) {
+    return Object.is(obj, targetValue);
+  }
+
+  // Recursive Case
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key];
+
+      if (Object.is(value, targetValue)) { return true;} 
+
+      // If the current value inside the object is an object or an array, it's recurse time lol
+      if (typeof value === 'object' && value !== null) {
+        if (contains(value, targetValue)) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
 };
   
 // Do not edit below this line
