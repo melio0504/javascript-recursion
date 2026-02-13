6018801c2536436e86075a48626e127eaa405a85
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-02-14 01:07:50 +0800

    Make the factorial function more better

--- 1_factorial/factorial.js
+++ 1_factorial/factorial.js
@@ -1,12 +1,10 @@
 const factorial = function(n) {
   // Do not accept the following
   // Non-positive integer || Float number
-  if (n < 0 || !Number.isInteger(n)) { return undefined };  
+  if (n < 0 || !Number.isInteger(n)) { return };
 
   // Recursive Base
-  if (n === 0) {
-    return 1;
-  }
+  if (n === 0) { return 1 };
 
   // Recursive Case
   return n * factorial(n - 1);
