276face6301cc2ba17fd9ac6bab8444f9b0f8e1b
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-02-14 14:48:04 +0800

    Create pascal function

--- 5_pascal/pascal.js
+++ 5_pascal/pascal.js
@@ -1,6 +1,19 @@
-const pascal = function() {
+const pascal = function (n) {
+  const currentRow = [1];
   
-};
+  // Recursive Base
+  if (n === 1) { return currentRow };
+
+  const previousLine = pascal(n - 1);
   
+  // Recursive Case
+  previousLine.forEach((number, i) => {
+    const rightNeighbor = previousLine[i + 1] ?? 0;
+    currentRow.push(number + rightNeighbor);
+  })
+
+  return currentRow;
+}
+
 // Do not edit below this line
 module.exports = pascal;
