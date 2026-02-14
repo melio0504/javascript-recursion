f4b4f7c1c8664fd8f6c72c362c03deebe2cfcf71
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-02-14 14:02:11 +0800

    Create totalIntegers function

--- 3_totalIntegers/totalIntegers.js
+++ 3_totalIntegers/totalIntegers.js
@@ -1,5 +1,23 @@
-const totalIntegers = function() {
+const isObject = (value) => typeof value === 'object' && value !== null;
+
+const totalIntegers = function(arr) {  
   
+  if (!isObject(arr)) { return };
+
+  const eachElement = Object.values(arr);
+
+  let total = 0;
+
+  for (const value of eachElement) {
+    if (Number.isInteger(value)) {
+      total++;
+    } else if (isObject(value)) {
+     // Recursive Case
+     total += totalIntegers(value);
+    }
+  }
+
+  return total;
 };
   
 // Do not edit below this line
