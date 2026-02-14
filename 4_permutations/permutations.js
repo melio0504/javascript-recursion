4160981783ebd24778f4cf9305342fbc6c470a6f
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-02-14 14:37:55 +0800

    Create permutations function

--- 4_permutations/permutations.js
+++ 4_permutations/permutations.js
@@ -1,6 +1,23 @@
-const permutations = function() {
-  
+const permutations = function (arr, index = 0, results = []) {
+  // Recursive Base
+  if (index == arr.length) {
+    // Cloning array inside results
+    results.push([...arr]);
+
+    return results;
+  }
+
+  // Recursive Case
+  for (let i = index; i < arr.length; i++) {
+    [arr[index], arr[i]] = [arr[i], arr[index]];
+
+    permutations(arr, index + 1, results);
+
+    [arr[index], arr[i]] = [arr[i], arr[index]];
+  }
+
+  return results;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
