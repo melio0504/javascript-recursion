67658f04670e0b215cd55d3827b8b2660a929dab
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-02-28 14:30:14 +0800

    Create mergeSort function

---
+++ 7_merge_sort/mergeSort.js
@@ -0,0 +1,37 @@
+function merge(left, right) {
+  const result = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i), right.slice(j));
+}
+
+function mergeSort(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Input must be an array okieee?");
+  }
+
+  // Base case
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+export { mergeSort };
